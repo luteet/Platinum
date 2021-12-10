2dbb472bf6ac02ecce607ae88ebe8ea0e3ec3cba
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2021-12-10 21:04:28 +0200

    v1.0

--- js/main.min.js
+++ js/main.min.js
@@ -35,6 +35,20 @@ body.addEventListener('click', function (e) {
         })
     }
 
+    if(e.target.closest('._btn-to-scroll')) {
+    
+        e.preventDefault();
+        menu.forEach(elem => {
+            elem.classList.remove('_active')
+        })
+        window.scroll({
+            left: 0, 
+            top: 0, 
+            behavior: 'smooth'
+        })
+    
+    }
+
     if(e.target.closest('._header-drop-down-btn') && !e.target.closest('._header-drop-down-btn._active')) {
         e.preventDefault();
 
@@ -61,21 +75,96 @@ function is_touch_device() {
 }
 
 
+new Swiper('.recommend-block__slider', {
+  
+    spaceBetween: 30,
+    slidesPerView: 1,
+    centeredSlides: false,
+    /* grabCursor: true, */
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    /* navigation: {
+      nextEl: '.recommend-block__arrow._next',
+      prevEl: '.recommend-block__arrow._prev',
+    }, */
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+        centeredSlides: true,
+    
+      },
+      600: {
+        slidesPerView: 2,
+        centeredSlides: false,
+      },
+      /* 768: {
+        centeredSlides: true,
+        spaceBetween: 15,
+        slidesPerView: 3,
+    
+      } */
+    }
+});
 
-let image = document.querySelector('.about-us__bg--elem-img'),
-    point = document.querySelector('.about-us__map-point');
+// КАСТОМНЫЙ input range {
 
-//console.log(image.clientHeight);
+    const filterRange = document.querySelector('.filter__price--range'),
+        filterRangeBody = document.querySelector('.filter__price--range-body'),
+        filterPriceMin = document.querySelector('.filter__price--value._min-value'),
+        filterPriceMax = document.querySelector('.filter__price--value._max-value'),
+        filterRangeValue = document.querySelector('.filter__price--range-value');
+try {
+    
+    const filterRangeSlider = noUiSlider.create(filterRangeBody, {
+        tooltips: true,
+        
+        start: [Number(filterRange.getAttribute('data-start'))],
+        connect: 'lower',
+        /* format: wNumb({
+            decimals: 0
+        }), */
+        step: Number(filterRange.getAttribute('step')),
+        range: {
+            'min': Number(filterPriceMin.getAttribute('min')),
+            'max': Number(filterPriceMax.getAttribute('max')),
+        },
+        
+        format: {
+            to: function (value) {
+                let valueString = Math.round(value).toString(),
+                    valueResult = '';
+      
+                    filterRange.setAttribute('value', valueString);
+                
+                if(valueString.length == 4) {
+                    valueResult = valueString.slice(0, 1) + " " + valueString.slice(1);
+                } else if(valueString.length == 5) {
+                    valueResult = valueString.slice(0, 2) + " " + valueString.slice(2);
+                } else if(valueString.length == 6) {
+                    valueResult = valueString.slice(0, 3) + " " + valueString.slice(3);
+                }
+                return (valueResult) ? valueResult : valueString;
+                
+            },
+            from: function (value) {
+                return Math.round(value);
+            }
+        }
+    
+    });
 
-image.addEventListener('load', function() {
-    //console.log('loaded');
+    filterRange.classList.add('_custome-slider-active');
+    
+} catch {}
 
-   /*  let coords = [image.clientWidth / 2.12, image.clientHeight / 2.8];
-
-    point.style.left = coords[0] + 'px';
-    point.style.top = coords[1] + 'px'; */
-    //console.log(coords);
-})
+// }
 
 
 
@@ -130,10 +219,30 @@ image.addEventListener('load', function() {
         });
     }
 
+    let pageAddress = document.querySelector('.page-address'),
+        pageAddressInit = document.querySelector('.page-address-init'),
+        pageAddressPlace = document.querySelector('._page-address-place');
+
       function resize() {
         
       windowSize = window.innerWidth;
     
+      resizeCheckFunc(992, 
+        function () {
+    
+            if(pageAddress) {
+                pageAddressInit.append(pageAddress);
+            }
+            
+        },
+        function () {
+          
+            if(pageAddress) {
+                pageAddressPlace.append(pageAddress);
+            }
+          
+        });
+
       resizeCheckFunc(768, 
         function () {
     
