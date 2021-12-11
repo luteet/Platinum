e7932889e17068b0e1160a78df4995675d938d7f
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2021-12-11 20:48:59 +0200

    v1.0

--- js/main.min.js
+++ js/main.min.js
@@ -1,5 +1,246 @@
 
 
+function slide(target, type) {
+    
+    let slideUp = (target, duration=500) => {
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.boxSizing = 'border-box';
+        target.style.height = target.offsetHeight + 'px';
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout( () => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        }, duration);
+    }
+
+    let slideDown = (target, duration=500) => {
+        target.style.removeProperty('display');
+        let display = window.getComputedStyle(target).display;
+
+        if (display === 'none')
+        display = 'block';
+
+        target.style.display = display;
+        let height = target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.boxSizing = 'border-box';
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        window.setTimeout( () => {
+        target.style.removeProperty('height');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        }, duration);
+    }
+
+    let checkSlideItem = false;
+    let slideToggle = (target, duration = 500) => {
+        if (window.getComputedStyle(target).display === 'none' && checkSlideItem == false) {
+            checkSlideItem = true;
+            setTimeout(function() {
+                checkSlideItem = false;
+            },500)
+        return slideDown(target, duration);
+        
+        } else {
+            checkSlideItem = true;
+            setTimeout(function() {
+                checkSlideItem = false;
+            },500)
+        return slideUp(target, duration);
+        }
+    }
+
+    if(type == 'up') {
+
+
+        slideUp(target);
+
+
+    } else if(type == 'down') {
+
+
+        slideDown(target);
+
+
+
+    } else if(type == 'toggle') {
+
+
+        slideToggle(target);
+
+
+    }
+
+}
+
+let widthScrollBar;
+
+function fade(element, type, duration) {
+
+    if(type == 'show') {
+        let opacity = 0,
+            intervalID = setInterval(function() {
+            if(element.style.visibility != 'visible') {
+                element.style.visibility = 'visible';
+            }
+            if (opacity < 1) {
+                opacity = opacity + 0.05
+                element.style.opacity = opacity;
+            } else {
+                clearInterval(intervalID);
+            }
+        }, (duration) ? duration : 10);
+
+    } else if(type == 'hide') {
+        let opacity = 1,
+            intervalID = setInterval(function() {
+
+            if (opacity > 0.01) {
+                opacity = opacity - 0.05
+                element.style.opacity = opacity;
+            } else {
+                clearInterval(intervalID);
+                element.style.visibility = 'hidden';
+            }
+        }, (duration) ? duration : 10);
+    }
+
+
+}
+
+
+
+/* document.querySelectorAll('._popup').forEach(element => {
+    element.style.visibility = 'visible';
+
+    setTimeout(() => {
+        element.style.visibility = '';
+    },1000)
+}) */
+
+function popup(arg) {
+
+    let popup, popupBg, popupCloseBtn,
+    
+        body    = arg.body,
+        html    = arg.html,
+        id      = arg.id;
+
+    try {
+        
+        popup = document.querySelector(id);
+        popupBg = popup.querySelector('._popup-bg');
+        popupCloseBtn = popup.querySelector('._popup-close-btn');
+
+    } catch {
+        return false;
+    }
+
+    function removeFunc(popup, duration) {
+
+        /* fade(popup, 'hide'); */
+        popup.classList.remove('_active');
+        setTimeout(function() {
+            
+            body.classList.remove('_popup-active');
+            html.style.setProperty('--popup-padding', '0px');
+        },(duration) ? duration : 200)
+        
+    }
+
+    const popupActive = document.querySelectorAll('._popup._active');
+
+    let popupActiveCheck = (popupActive) ? false : true;
+
+    popupActive.forEach(element => {
+        element.classList.remove('_active');
+        //fade(popup, 'hide');
+        /* body.classList.add('_popup-active');
+        html.style.setProperty('--popup-padding',  + 17 + 'px'); */
+    })
+
+    body.classList.remove('_popup-active');
+    html.style.setProperty('--popup-padding', window.innerWidth - body.offsetWidth + 'px');
+    body.classList.add('_popup-active');
+
+    
+    
+    //window.location.hash = id;
+
+    popup.classList.add('_active');
+    //fade(popup, 'show');
+
+    
+
+    popupBg.addEventListener('click', function() {
+        removeFunc(popup);
+        setTimeout(function() {
+            return false;
+        },200)
+    });
+
+    popupCloseBtn.addEventListener('click', function() {
+        removeFunc(popup);
+        setTimeout(function() {
+            return false;
+        },200)
+    });
+
+}
+/* 
+function popupInit() {
+    let hash = window.location.hash,
+        popupElem;
+
+    try {
+        popupElem = document.querySelector(hash);
+    } catch {
+        return false;
+    }
+
+    if(popupElem.classList.contains('_popup')) {
+        popup(hash);
+    }
+    
+}
+
+popupInit(); */
+
+
+
+
+
+
+
+
+
 const body = document.querySelector('body'),
     html = document.querySelector('html'),
     menu = document.querySelectorAll('._burger, .header__nav, body'),
@@ -25,15 +266,173 @@ body.addEventListener("mouseout", function(e) {
     
 }, false); */
 
+const   filterDropDownLabel = document.querySelectorAll('._has-filter-drop-down-list'),
+        filterFormRadioLabel = document.querySelectorAll('.filter__form--item-radio-label');
+
+function filterRadioLabelFunc(element) {
+
+    if(!element.classList.contains('_active')) {
+        filterFormRadioLabel.forEach(element => {
+
+            if(element.classList.contains('_has-filter-drop-down-list')) {
+
+                let filterDropDownList = element.parentNode.querySelector('._filter-drop-dowm-list._active');
+                
+                if(filterDropDownList) {
+                    slide(filterDropDownList, 'up');
+                    filterDropDownList.classList.remove('_active');
+                }
+                
+            }
+            
+            element.classList.remove('_active');
+            
+        });
+
+        element.classList.add('_active');
+
+
+        if(element.classList.contains('_has-filter-drop-down-list')) {
+            console.log(element.parentNode.querySelector('._filter-drop-dowm-list'));
+            const dropDownList = element.parentNode.querySelector('._filter-drop-dowm-list');
+    
+            dropDownList.classList.add('_active');
+            
+            slide(dropDownList, 'down');
+
+        }
+
+    }
+
+}
+
+filterFormRadioLabel.forEach(element => {
+
+    if(element.classList.contains('_active')) {
+        element.querySelector('input').checked = true;
+        filterRadioLabelFunc(element);
+    }
+
+    element.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        this.querySelector('input').checked = true;
+
+        filterRadioLabelFunc(this);
+        
+    });
+
+});
+
+
+
+const   filter          = document.querySelector('.filter'),
+        filterBg        = document.querySelector('.filter__bg'),
+        filterOpenBtn   = document.querySelector('._filter-open-btn');
+
 body.addEventListener('click', function (e) {
 
+    //console.log(e.target);
     // Меню в шапке
+
+
+
+
+    if(e.target.closest('._btn-plus')) {
+        const inputNumber = e.target.closest('._btn-plus').parentNode.querySelector('._input-number');
+
+        inputNumber.value = Number(inputNumber.value) + 1;
+    }
+
+    if(e.target.closest('._btn-minus')) {
+        const inputNumber = e.target.closest('._btn-minus').parentNode.querySelector('._input-number');
+
+        if(Number(inputNumber.value) != 1) inputNumber.value = Number(inputNumber.value) - 1;
+        
+    }
+
+    if(e.target.closest('._review-more-link')) {
+        e.preventDefault();
+
+        const thisElem = e.target.closest('._review-more-link'),
+              reviewParent = thisElem.closest('._review-item');
+        
+        if(!reviewParent.classList.contains('_active')) {
+            thisElem.classList.add('_active');
+            reviewParent.classList.add('_active');
+        }
+
+    }
+
+    
+
+
+
+
+    if(e.target.closest('._popup-btn')) {
+        e.preventDefault();
+        popup({
+            id: e.target.closest('._popup-btn').getAttribute('href'),
+            html: html,
+            body: body,
+        });
+    }
+
+
+
+
+
+
+    if(e.target.closest('.filter__form--item-radio-checkbox-label')) {
+        e.preventDefault();
+        
+        const checkboxLabel         = e.target.closest('.filter__form--item-radio-checkbox-label'),
+              checkboxLabelInput    = checkboxLabel.querySelector('input');
+
+
+        checkboxLabelInput.checked = !checkboxLabelInput.checked;
+
+
+        if(checkboxLabelInput.checked) checkboxLabel.classList.add('_active'); else checkboxLabel.classList.remove('_active');
+
+    }
+
+    if(e.target.closest('._filter-open-btn')) {
+        e.preventDefault();
+        if(!filter.classList.contains('_active')) {
+            filter.classList.add('_active');
+
+            menu.forEach(elem => {
+                elem.classList.remove('_active')
+            })
+            body.classList.add('_filter-active');
+        }
+    }
+
+    if(e.target.closest('.filter__bg') || e.target.closest('.filter__header--close-btn')) {
+
+        if(filter.classList.contains('_active')) {
+            filter.classList.remove('_active')
+            body.classList.remove('_filter-active');
+        }
+    }
+
+
+
+
+
+    
     if (e.target.closest('._burger')) {
         
         menu.forEach(elem => {
             elem.classList.toggle('_active')
         })
     }
+
+
+
+
+
 
     if(e.target.closest('._btn-to-scroll')) {
     
@@ -48,6 +447,11 @@ body.addEventListener('click', function (e) {
         })
     
     }
+
+
+
+
+
 
     if(e.target.closest('._header-drop-down-btn') && !e.target.closest('._header-drop-down-btn._active')) {
         e.preventDefault();
@@ -67,6 +471,16 @@ body.addEventListener('click', function (e) {
             element.classList.remove('_active')
         });
     }
+
+
+
+
+
+
+
+
+
+
 
 })
 
@@ -132,8 +546,8 @@ try {
         }), */
         step: Number(filterRange.getAttribute('step')),
         range: {
-            'min': Number(filterPriceMin.getAttribute('min')),
-            'max': Number(filterPriceMax.getAttribute('max')),
+            'min': Number(filterRange.getAttribute('min')),
+            'max': Number(filterRange.getAttribute('max')),
         },
         
         format: {
@@ -154,6 +568,11 @@ try {
                 
             },
             from: function (value) {
+                filterRangeBody.insertAdjacentHTML('beforeend', 
+
+                `<span class="range-value range-value-min">${Number(filterRange.getAttribute('min'))}</span>
+                <span class="range-value range-value-max">${Number(filterRange.getAttribute('max'))}</span>`)
+                
                 return Math.round(value);
             }
         }
