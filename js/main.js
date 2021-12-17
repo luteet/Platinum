99b7fec5a9bcee12017cb00a876b067017fa0624
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2021-12-17 12:25:37 +0200

    v1.0

--- js/main.js
+++ js/main.js
@@ -929,6 +929,61 @@ function hiddenToggle(id) {
       
       // Media запросы }
 
+   
+      function getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+    
+        return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+        };
+    
+    }
+    
+    
+    function scrollPage() {
+    
+    const offsetCheckJs = document.querySelector('.offset-check-js');
+    let top = [getCoords(offsetCheckJs).top, false];
+    
+    header.classList.add('_loaded');
+    
+    function scrollPageFunc() {
+      top[0] = getCoords(offsetCheckJs).top;
+      
+      if(top[0] >= 300 && top[1] == false) {
+    
+          top[1] = true;
+          header.style.setProperty('--pos', '-100%');
+    
+          setTimeout(function() {
+              header.classList.add('_active');
+              header.style.setProperty('--pos', '0%');
+          },200);
+    
+      } else if(top[0] <= 300 && top[1] == true) {
+    
+          top[1] = false;
+          header.style.setProperty('--pos', '-100%');
+    
+          setTimeout(function() {
+              header.style.setProperty('--pos', '0%');
+              header.classList.remove('_active');
+              
+          },200);
+    
+      }
+    }
+    
+    scrollPageFunc();
+    
+    window.onscroll = scrollPageFunc;
+    
+    }
+    
+    scrollPage();
+    
+
 
 /* // Анимация {
 
